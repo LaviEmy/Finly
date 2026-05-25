142ab3d7635d2e19bb511c6baa4ed75d07465d3b
Ether <marekkeb9@gmail.com>
2026-05-25 23:16:16 +0200

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
 }
